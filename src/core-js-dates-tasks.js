a54c1cf0c6c03108ee8976cb80a003e2a992a50f
Mark <m.web.jan@gmail.com>
2024-03-22 01:34:54 +0300

    refactor: complete getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -209,8 +209,15 @@ function getWeekNumberByDate(/* date */) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const d = new Date(date);
+  d.setDate(13);
+  const a = true;
+  while (a) {
+    if (d.getDay() === 5) break;
+    d.setMonth(d.getMonth() + 1);
+  }
+  return d;
 }
 
 /**
